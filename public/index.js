4e378a63ae1e07da1063506ad7638d21c61437ee
David Rawson <dm.rawson@gmail.com>
2018-02-21 16:01:13 +0000

    Irritating bug eliminated.

--- public/index.js
+++ public/index.js
@@ -1,39 +1,24 @@
 const  app = function(){
-  // console.log("onload called");
   const url = "https://restcountries.eu/rest/v2/all";
-
-  // // requestComplete is a callback for when the request is um completed.
-  // button.addEventListener("click", handleButtonClicked)
   makeRequest(url, requestComplete);
 }
 
 const makeRequest = function(url, callback){
   const request = new XMLHttpRequest();
   request.open("GET", url);
-  // Remember, you can't do anything with the data until you have it.
-  // The callback, requestComplete will fire up once loaded.
   request.addEventListener("load", callback);
-  // THIS is an easy step to miss out.
   request.send();
 }
 
 const requestComplete = function(){
-  // console.log("callback called - requestComplete");
-  // .this here refers to request from the eventlistener line.
   if(this.status !== 200) return;
   const jsonString = this.responseText;
-  // console.log(jsonString);
-  // Parse the string into an array of objects
 
   let savedCountry = JSON.parse(localStorage.getItem('country'))
-  // console.log("savedCoutry", savedCountry);
-  // stringCountry = JSON.parse(savedCountry);
-  // console.log(stringCountry);
   displayCountryDetails(savedCountry);
 
   const countries = JSON.parse(jsonString);
   populateDropDown(countries);
-  // populateList(countries);
 }
 
 const populateDropDown = function(countries){
@@ -56,27 +41,21 @@ const populateDropDown = function(countries){
   }
 
   const displayCountryDetails = function(country){
-    // li_clearer();
-
-
-    // console.log(country.name, country.population, country.capital);
     const ul = document.getElementById("country-detail-list");
+    ul.innerHTML = "";
     const li_name = document.createElement("li");
     li_name.setAttribute("id", "li_name");
-    li_name.innerText = " ";
     li_name.innerText = "Country: " + country.name;
     const li_capital = document.createElement("li");
     li_capital.setAttribute("id", "li_capital");
-    li_capital.innerText = " ";
     li_capital.innerText = "Capital: " + country.capital;
     const li_population = document.createElement("li");
     li_population.setAttribute("id", "li_population");
-    li_population.innerText = " ";
     li_population.innerText = "Population: " + country.population;
-    // ul.clear();
     ul.appendChild(li_name);
     ul.appendChild(li_capital);
     ul.appendChild(li_population);
+    console.log(ul.innerHTML);
 
     persistCountry(country);
   }
